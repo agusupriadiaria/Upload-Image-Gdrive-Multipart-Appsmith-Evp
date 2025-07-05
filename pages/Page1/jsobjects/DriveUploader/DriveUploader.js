96c41e9c782e95cc5e2f35abc077f039f6682893
agus supriadi <agus.supriadi.testing1@gmail.com>
2025-07-05 15:15:39 +0000

    update

---
+++ pages/Page1/jsobjects/DriveUploader/DriveUploader.js
@@ -0,0 +1,31 @@
+export default {
+	async uploadAllFiles () {
+		for (let file of FilePicker1.files) {
+			const base64 = file.data.split(",")[1]; // buang prefix "data:image/png;base64,"
+
+			const body = `--boundary123
+Content-Type: application/json; charset=UTF-8
+
+{
+  "name": "${file.name}",
+  "parents": ["1vzCZ3XpjPdPgACYNy97TQbmN21m5AFvr"]
+}
+
+--boundary123
+Content-Type: ${file.type}
+Content-Transfer-Encoding: base64
+
+${base64}
+--boundary123--`;
+
+			try {
+				await uploadToDrive.run({ body }); // Authorization dan headers otomatis dari datasource
+			} catch (e) {
+				showAlert(`Gagal upload: ${file.name}`, "error");
+				console.error(e);
+			}
+		}
+
+		showAlert("Semua file berhasil di-upload!", "success");
+	}
+}
